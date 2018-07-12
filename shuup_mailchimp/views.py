82a8d69ae7fa44600a83a04e6621235659b5b270
Panu Tulimäki <panu@shuup.com>
2018-07-11 18:57:22 -0700

    Update license and copyright

--- shuup_mailchimp/views.py
+++ shuup_mailchimp/views.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 # This file is part of Shuup.
 #
-# Copyright (c) 2012-2016, Shoop Commerce Ltd. All rights reserved.
+# Copyright (c) 2012-2018, Shuup Inc. All rights reserved.
 #
-# This source code is licensed under the AGPLv3 license found in the
+# This source code is licensed under the OSL-3.0 license found in the
 # LICENSE file in the root directory of this source tree.
-
 from django.core.validators import validate_email, ValidationError
 from django.http.response import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
